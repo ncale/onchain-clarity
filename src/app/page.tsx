2c3d6315dd9c76bc66465b2f715d817c55084789
ncale <nicholasbrodeur@outlook.com>
2024-03-04 11:50:00 -0700

    add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <></>
+    <main className="flex flex-col items-center gap-8 pt-10">
+      <p className="text-center text-xl w-1/2">We&apos;re creating open source solutions for bringing higher visibility to blockchain by providing a number of services related to social, governance, and more</p>
+      <p className="text-center text-xl w-1/2 font-extralight">
+        You can reach us anytime on Farcaster
+        <a href="https://warpcast.com/occ.eth" target="_blank" className="text-blue-500 hover:underline"> @occ.eth</a>
+        . We&apos;d love to hear your ideas or feedback!
+      </p>
+    </main>
   );
 }
