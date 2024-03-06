cd62b3ed1db138c2a07a152a12c662863f8d0343
ncale <nicholasbrodeur@outlook.com>
2024-03-06 11:00:01 -0700

    add link over header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
-import { Image } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, Image } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Header() {
 	return (
 		<Navbar height="6rem" isBordered maxWidth="full">
 			<NavbarBrand>
-				<Image src="/occ-logo.png" alt="Onchain clarity logo" height={80} className="h-28" />
-				<p className="text-inherit text-4xl font-extralight -ml-2">Onchain Clarity Co.</p>
+				<Link href="/" className="flex items-center">
+					<Image src="/occ-logo.png" alt="Onchain clarity logo" height={80} className="h-28" />
+					<p className="text-inherit text-4xl font-extralight -ml-2">Onchain Clarity Co.</p>
+				</Link>
 			</NavbarBrand>
 			<NavbarContent>
 				{/* link structure placeholder */}
