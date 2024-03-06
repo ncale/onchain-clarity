a4023fb886b9718f37be61e5ae0c2790dc4a3524
ncale <nicholasbrodeur@outlook.com>
2024-03-06 13:48:36 -0700

    center align home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Image } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="main">
+    <main className="main main-home">
 			<Image src="/images/occ-logo.png" alt="Onchain clarity logo" height={80} className="h-28" />
     </main>
   );
