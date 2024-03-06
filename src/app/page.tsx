b772fcc004405ac650000a72bb610ceb00dbf2c8
ncale <nicholasbrodeur@outlook.com>
2024-03-06 11:52:32 -0700

    add root page and about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import { Image } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className="main">
-
+			<Image src="/occ-logo.png" alt="Onchain clarity logo" height={80} className="h-28" />
     </main>
   );
 }
