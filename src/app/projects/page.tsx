9d4fe1b37a0aaf7f09d7e4a26c373f5082755e5a
ncale <nicholasbrodeur@outlook.com>
2024-03-06 12:30:01 -0700

    update projects page and project card component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,33 +1,27 @@
 import ProjectCard from "@/components/ProjectCard";
 
+const govScoreDesc = "A site that scores Optimism delegates based on transparency of identity, how consistently they vote, and how much voting power they currently have"
+const basedFriendsDesc = "Crypto's native friends list - BasedFriends uses the farcaster social graph to allow you to see, of your follows, who is recently online and recently onchain"
+
 export default function Home() {
   return (
     <main className="main">
-      <div className="flex flex-col gap-4 mb-10 mt-4">
-        <p className="text-center text-lg">
-          We&apos;re creating open source solutions for bringing
-          <span className="font-bold"> higher visibility to blockchain </span>
-          by providing a number of services related to social, governance, and more</p>
-        <p className="text-center text-lg">
-          You can reach us anytime on Farcaster
-          <a href="https://warpcast.com/occ.eth" target="_blank" className="text-blue-500 hover:underline"> @occ.eth</a>
-          . We&apos;d love to hear from you, whether you have ideas, feedback, or general thoughts about our mission.
-        </p>
-      </div>
-      <div className="flex flex-wrap gap-6 justify-center">
+			<section className="flex flex-col gap-2">
         <ProjectCard 
-          name="GovScore" 
+          name="GovScore"
+					desc={govScoreDesc}
           projLink="" 
           imgUrl="/based-friends-thumbnail.png" 
           imgAlt="Gov Score Thumbnail" 
           width={800} height={800} />
         <ProjectCard 
-          name="BasedFriends" 
+          name="BasedFriends"
+					desc={basedFriendsDesc}
           projLink="https://basedfriends.xyz" 
           imgUrl="/based-friends-thumbnail.png" 
           imgAlt="Based Friends Thumbnail" 
           width={800} height={800} />
-      </div>
+      </section>
     </main>
   );
 }
