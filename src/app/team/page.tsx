23bbe0d43229b94015cf2aa13c567da613babb97
ncale <nicholasbrodeur@outlook.com>
2024-03-06 13:27:58 -0700

    add team page and user cards

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,9 +1,26 @@
-
+import UserCard from "@/components/UserCard";
 
 export default function Home() {
   return (
     <main className="main">
-
+			<section className="grid grid-cols-2 gap-2 w-1/2">
+				<UserCard 
+					name="limes.eth"
+					secondaryName="a.k.a. Dylan"
+					pfpLink=""
+					fcLink=""
+					telegramLink=""
+					roles={["ENS Steward", "Tall DAO Member"]}
+					/>
+				<UserCard 
+					name="ncale.eth"
+					secondaryName="a.k.a. Nick"
+					pfpLink=""
+					fcLink=""
+					telegramLink=""
+					roles={["Full Stack Dev"]}
+					/>
+			</section>
     </main>
   );
 }
