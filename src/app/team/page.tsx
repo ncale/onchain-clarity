72c4a784a29ce290d6281e059b0688aa641900f7
ncale <nicholasbrodeur@outlook.com>
2024-03-06 15:41:58 -0700

    add telegram links

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 					secondaryName="a.k.a. Dylan"
 					pfpLink="https://i.seadn.io/gae/CsglYc_NZ0lHvdIHH9dXUpiAjL21jXvajIOaqRep64H76yzjsGjVa3z3KcZtNBPoeyuBizJywkWddxQ8t8gQB2sOjPny2WGFj4LEfg?w=500&auto=format"
 					fcLink="https://warpcast.com/limes.eth"
-					telegramLink=""
+					telegramLink="https://t.me/limes_eth"
 					roles={["ENS Steward", "Tall DAO Member"]}
 					/>
 				<UserCard 
@@ -17,7 +17,7 @@ export default function Home() {
 					secondaryName="a.k.a. Nick"
 					pfpLink="https://i.imgur.com/Sfbunk9.jpg"
 					fcLink="https://warpcast.com/ncale.eth"
-					telegramLink=""
+					telegramLink="https://t.me/nbrodeur"
 					roles={["Full Stack Dev"]}
 					/>
 			</section>
