2917c022b7df850b5e7ec6ee26c1c232d5e97cc6
ncale <nicholasbrodeur@outlook.com>
2024-03-25 14:29:44 -0500

    update project card component

--- src/app/projects/project-card.tsx
+++ src/app/projects/project-card.tsx
@@ -1,31 +1,34 @@
-import {Card, CardBody, Image } from "@nextui-org/react";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import Image from "next/image";
 
-export default function ProjectCard(props: { name: string, desc: string, projLink: string, imgUrl: string, imgAlt: string, width: number, height: number | undefined }) {
-	return (
-		<a href={props.projLink} target="_blank">
-			<Card
-				radius="lg"
-				className="border-none"
-			>
-				<CardBody>
-					<div className="grid grid-cols-3">
-						<div className="relative col-span-1 mr-3">
-							<h2 className="text-lg font-bold">{props.name}</h2>
-							<p className="text-md font-extralight">{props.desc}</p>
-						</div>
-						<div className="relative col-span-2 h-72 w-full">
-							<Image
-								src={props.imgUrl}
-								alt={props.imgAlt}
-								width={props.width}
-								height={props.height}
-								removeWrapper
-								className="z-0 w-full h-full object-cover object-left-top"
-							/>
-						</div>
-					</div>
-				</CardBody>
-			</Card>
-		</a>
-	)
+export default function ProjectCard(props: {
+  name: string;
+  desc: string;
+  projLink: string;
+  imgUrl: string;
+  imgAlt: string;
+}) {
+  return (
+    <a href={props.projLink} target="_blank">
+      <Card radius="lg" className="border-none">
+        <CardHeader>
+          <div>
+            <p className="text-lg font-bold">{props.name}</p>
+            <p className="text-md">{props.desc}</p>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <div className="relative w-80 h-48 rounded-md overflow-hidden mx-auto md:mx-0">
+            <Image
+              src={props.imgUrl}
+              alt={props.imgAlt}
+              width={800}
+              height={800}
+              className="z-0 w-full h-full object-cover object-left-top"
+            />
+          </div>
+        </CardBody>
+      </Card>
+    </a>
+  );
 }
