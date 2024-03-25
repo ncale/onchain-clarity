168b4db18360797ffc35ff96f5844ca4507e3376
ncale <nicholasbrodeur@outlook.com>
2024-03-25 14:43:13 -0500

    update socials page

--- src/app/socials/page.tsx
+++ src/app/socials/page.tsx
@@ -5,20 +5,20 @@ import pgIcon from "../../../public/icons/paragraph-icon.svg";
 export default function Home() {
   return (
     <main className="main">
-			<section className="flex flex-col gap-2">
-				<SocialsCard 
-					message="Follow us on Farcaster"
-					link="https://warpcast.com/occ.eth"
-					iconSrc={fcIcon}
-					iconAlt="Farcaster icon"
-					/>
-				<SocialsCard 
-					message="Subscribe to our Paragraph blog"
-					link="https://paragraph.xyz/@occ.eth"
-					iconSrc={pgIcon}
-					iconAlt="Paragraph icon"
-					/>
-			</section>
+      <section className="flex flex-col gap-4 pt-4 md:items-center md:w-[32em]">
+        <SocialsCard
+          message="Follow us on Farcaster"
+          link="https://warpcast.com/occ.eth"
+          iconSrc={fcIcon}
+          iconAlt="Farcaster icon"
+        />
+        <SocialsCard
+          message="Subscribe to our Paragraph blog"
+          link="https://paragraph.xyz/@occ.eth"
+          iconSrc={pgIcon}
+          iconAlt="Paragraph icon"
+        />
+      </section>
     </main>
   );
 }
